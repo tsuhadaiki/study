0963e94d174b65b8f868a0d82e416bcfa7422034
tsuhadaiki <e175730@ie.u-ryukyu.ac.jp>
2019-05-09 15:09:16 +0900

    add base code

---
+++ regression.py
@@ -0,0 +1,11 @@
+import numpy as np
+class LinearRegression: 
+    x = None
+    theta = None 
+    y = None
+    def fit(self, x, y): 
+        pass
+    def predict(self, x): 
+        pass
+    def score(self, x, y): 
+        pass
