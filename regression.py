134f44372d9166da45a2c27963767c3a14f03ad2
tsuhadaiki <e175730@ie.u-ryukyu.ac.jp>
2019-05-09 15:21:07 +0900

    add function fit

--- regression.py
+++ regression.py
@@ -4,8 +4,9 @@ class LinearRegression:
     theta = None 
     y = None
     def fit(self, x, y): 
-        pass
+        temp=np.linalg.inv(np.dot(x.T,x))
+        self.theata = np.dot(np.dot(temp,x.T),y)
     def predict(self, x): 
-        pass
+        passimport
     def score(self, x, y): 
         pass
