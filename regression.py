dcf9573613c61f2afaa58062672818ecf9cfcf70
tsuhadaiki <e175730@ie.u-ryukyu.ac.jp>
2019-05-09 15:34:16 +0900

    add function score

--- regression.py
+++ regression.py
@@ -9,4 +9,5 @@ class LinearRegression:
     def predict(self, x): 
         return np.dot(x, self.theta)
     def score(self, x, y): 
-        pass
+        error = self.predict(x) - y 
+        return (error**2).sum()
