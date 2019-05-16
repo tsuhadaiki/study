d50a52f8c38d60dae1ea0487c51d2560331027fb
tsuhadaiki <e175730@ie.u-ryukyu.ac.jp>
2019-05-16 15:44:33 +0900

    add non_liner data and polynomial

--- datasets.py
+++ datasets.py
@@ -11,7 +11,22 @@ import numpy as np
 def load_liner_example1():
     X = np.array([[1,4],[1,8],[1,13],[1,17]]) 
     Y = np.array([7, 10, 11, 14])
-    return X, Y
+    alpha = 0.1
+    return X, Y, alpha
+def load_nonliner_example1():
+    X = np.array([[1,0.0],[1,2.0],[1,3.9],[1,4.0]]) 
+    Y = np.array([4.0, 0.0, 3.0, 2.0])
+    alpha = 0.1
+    return X, Y, alpha
+
+def polynomial2_features(input): 
+    poly2 = input[:,1:]**2
+    return np.c_[input, poly2]
+
+def polynomial3_features(input): 
+    poly2 = input[:,1:]**2
+    poly3 = input[:,1:]**3
+    return np.c_[input, poly2, poly3]
 
 if __name__ == '__main__':
     import doctest
