382b7b1d83845c156cbb07dbe1f42ae9a4015c32
tsuhadaiki <e175730@ie.u-ryukyu.ac.jp>
2019-05-16 15:51:38 +0900

    remove load_nonliner_example1() of alpha

--- datasets.py
+++ datasets.py
@@ -16,8 +16,7 @@ def load_liner_example1():
 def load_nonliner_example1():
     X = np.array([[1,0.0],[1,2.0],[1,3.9],[1,4.0]]) 
     Y = np.array([4.0, 0.0, 3.0, 2.0])
-    alpha = 0.1
-    return X, Y, alpha
+    return X, Y
 
 def polynomial2_features(input): 
     poly2 = input[:,1:]**2
