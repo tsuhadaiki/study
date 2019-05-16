3004eca858f30525cb90e4a2afc5614f09de06e2
tsuhadaiki <e175730@ie.u-ryukyu.ac.jp>
2019-05-16 15:46:07 +0900

    add base

---
+++ nonlinear_ex.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import datasets
+import regression
+X, Y = datasets.load_nonliner_example1()
+ex_X = datasets.polynomial2_features(X) 
+model = regression.LinearRegression() 
+model.fit(ex_X, Y)
+samples = np.arange(0, 4, 0.1)
+x_samples = np.c_[ np.ones(len(samples)), samples ] 
+ex_x_samples = datasets.polynomial2_features(x_samples)
+import matplotlib.pyplot as plt 
+plt.scatter(X[:,1], Y)
+plt.plot(samples, model.predict(ex_x_samples)) 
+plt.show()
